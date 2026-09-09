43eabf21cd505a734a2300b05ae4decbd5ce6a04
Geraldo Luiz <geraldobl58@gmail.com>
2026-09-09 18:39:52 -0300

    Refactor TestLLMCall to enhance chat response handling and output metadata

--- src/main/java/com/geraldoluiz/medassistant/TestLLMCall.java
+++ src/main/java/com/geraldoluiz/medassistant/TestLLMCall.java
@@ -1,10 +1,13 @@
 package com.geraldoluiz.medassistant;
 
 import lombok.RequiredArgsConstructor;
-import org.jspecify.annotations.NonNull;
 import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.util.Objects;
 
 @Component
 @RequiredArgsConstructor
@@ -12,8 +15,19 @@ public class TestLLMCall implements CommandLineRunner {
     private final ChatModel chatModel;
 
     @Override
-    public void run(String @NonNull ... args) throws Exception {
-        String response = chatModel.call("Hello, how are you?");
-        System.out.println(response);
+    public void run(String... args) throws Exception {
+        ChatResponse chatResponse = chatModel.call(new Prompt("Hello, how are you?"));
+
+
+        String content = Objects.requireNonNull(chatResponse.getResult().getOutput().getText());
+        System.out.println("Chat response");
+        System.out.println(content);
+
+        System.out.println("\nMetadata");
+        System.out.println("Model " + chatResponse.getMetadata().getModel());
+        System.out.println("Tokens " + chatResponse.getMetadata().getUsage().getCompletionTokens());
+        System.out.println("Total de tokens " + chatResponse.getMetadata().getUsage().getTotalTokens());
+
+        System.out.println("Finished reason: " + chatResponse.getResult().getMetadata().getFinishReason());
     }
 }
